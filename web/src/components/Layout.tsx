3313f7a55141b89074bced72a4e391418a063a03
Luke Harris <lukeharris01170@gmail.com>
2021-04-29 20:06:06 +0100

    Add footer

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -5,6 +5,11 @@ const Layout = ({ children, ...rest }) => (
   <div {...rest}>
     <GlobalStyles />
     <div tw="container mx-auto px-4">{children}</div>
+    <footer tw="bg-gray-100 mt-8">
+      <div tw="container mx-auto px-4 py-8">
+        Copyright &copy; Luke Harris {new Date().getFullYear()}
+      </div>
+    </footer>
   </div>
 );
 
